6513f1269b760bf53e10162e9ec84a6f8df58fd7
eliezerjoel <kagulirejoeleliezer@gmail.com>
2024-06-28 10:19:20 +0300

    modified joel route params

--- app/api/joel/route.ts
+++ app/api/joel/route.ts
@@ -1,11 +1,12 @@
 import {NextResponse} from 'next/server'
 
 export const GET = async ()=>{
-    return new NextResponse(JSON.stringify({message: "This is Kagu's first api complete in Nextjs and deployed on Vercel"}))
+    return new NextResponse(JSON.stringify({message: "An api complete in Nextjs and deployed on Vercel"}))
 }
 export const POST = async (request: Request)=>{
-    const body = await request.json()
-    const name = body.name
+    const {searchParams} = new URL(request.url)
+    
+    const name = searchParams.get("name")
 
     return new NextResponse(name)
 }
