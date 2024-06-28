f339fad9d4c269151d01dbd154248c8b8da64467
eliezerjoel <kagulirejoeleliezer@gmail.com>
2024-06-28 09:37:38 +0300

    modified joel route

--- app/api/joel/route.ts
+++ app/api/joel/route.ts
@@ -1,1 +1,4 @@
-// export const GET
+import {NextResponse} from 'next/server'
+export const GET = async ()=>{
+    return new NextResponse(JSON.stringify({message: "This is Kagu's first api complete in Nextjs and deployed on Vercel"}))
+}
