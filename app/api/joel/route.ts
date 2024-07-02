0c6484c676ca9e6972a912a713e21e0c09ed0d4b
eliezerjoel <kagulirejoeleliezer@gmail.com>
2024-07-02 14:51:08 +0300

    added proces.env

--- app/api/joel/route.ts
+++ app/api/joel/route.ts
@@ -1,7 +1,7 @@
 import {NextResponse} from 'next/server'
 
 export const GET = async ()=>{
-    return new NextResponse(JSON.stringify({message: "An api complete in Nextjs and deployed on Vercel"}))
+    return new NextResponse(JSON.stringify({message: "An api complete in Nextjs and deployed on Vercel" + process.env.MONGODB_URI}))
 }
 export const POST = async (request: Request)=>{
     const {searchParams} = new URL(request.url)
